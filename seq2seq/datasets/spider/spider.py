273b67963f144d2f204b58b70e097bf97b5d6352
elena-soare <91671393+elena-soare@users.noreply.github.com>
2022-06-18 10:10:21 +0100

    Update spider.py

--- seq2seq/datasets/spider/spider.py
+++ seq2seq/datasets/spider/spider.py
@@ -41,7 +41,7 @@ _HOMEPAGE = "https://yale-lily.github.io/spider"
 
 _LICENSE = "CC BY-SA 4.0"
 
-_URL = "https://drive.google.com/uc?export=download&id=1_AckYkinAnhqmRQtGsQgUKAnTHxxX5J0&confirm=t"
+_URL = "https://drive.google.com/uc?export=download&id=1PGzc_6p7tLq8Mbu67zNhWg6rCAUnpbF1&confirm=t"
 
 
 class Spider(datasets.GeneratorBasedBuilder):
