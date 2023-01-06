d15d2a0084019f57d3a263a2a4e9da13be0b686c
Torrin McQueen <60006667+totem37@users.noreply.github.com>
2023-01-06 11:08:07 +0000

    Update torch_module.py

--- seq2seq/utils/torch_module.py
+++ seq2seq/utils/torch_module.py
@@ -17,12 +17,53 @@ class Module(Module):
         """
         if prefix == 'module.':
             prefix = ''
-        for name, param in self._parameters.items():
-            if param is not None:
-                destination[prefix + name] = param if keep_vars else param.detach()
-        for name, buf in self._buffers.items():
-            if buf is not None and name not in self._non_persistent_buffers_set:
-                destination[prefix + name] = buf if keep_vars else buf.detach()
+        super()._save_to_state_dict(destination, prefix, keep_vars)
+#         for name, param in self._parameters.items():
+#             if param is not None:
+#                 destination[prefix + name] = param if keep_vars else param.detach()
+#         for name, buf in self._buffers.items():
+#             if buf is not None and name not in self._non_persistent_buffers_set:
+#                 destination[prefix + name] = buf if keep_vars else buf.detach()
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict,
+                              missing_keys, unexpected_keys, error_msgs):
+        r"""Copies parameters and buffers from :attr:`state_dict` into only
+        this module, but not its descendants. This is called on every submodule
+        in :meth:`~torch.nn.Module.load_state_dict`. Metadata saved for this
+        module in input :attr:`state_dict` is provided as :attr:`local_metadata`.
+        For state dicts without metadata, :attr:`local_metadata` is empty.
+        Subclasses can achieve class-specific backward compatible loading using
+        the version number at `local_metadata.get("version", None)`.
+
+        .. note::
+            :attr:`state_dict` is not the same object as the input
+            :attr:`state_dict` to :meth:`~torch.nn.Module.load_state_dict`. So
+            it can be modified.
+            
+        This method has been overridden to ensure models wrapped in a module can
+        be loaded.
+
+        Args:
+            state_dict (dict): a dict containing parameters and
+                persistent buffers.
+            prefix (str): the prefix for parameters and buffers used in this
+                module
+            local_metadata (dict): a dict containing the metadata for this module.
+                See
+            strict (bool): whether to strictly enforce that the keys in
+                :attr:`state_dict` with :attr:`prefix` match the names of
+                parameters and buffers in this module
+            missing_keys (list of str): if ``strict=True``, add missing keys to
+                this list
+            unexpected_keys (list of str): if ``strict=True``, add unexpected
+                keys to this list
+            error_msgs (list of str): error messages should be added to this
+                list, and will be reported together in
+                :meth:`~torch.nn.Module.load_state_dict`
+        """
+        if prefix == 'module.':
+            prefix = ''
+        super()._load_from_state_dict(state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs)
         
 class DistributedDataParallel(Module, DistributedDataParallel):
     pass
