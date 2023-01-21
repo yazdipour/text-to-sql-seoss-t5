9974065493b4a17c810710b39e6f92abb461f19c
Shahriar Yazdipour <yazdipour@users.noreply.github.com>
2023-01-21 02:32:53 +0100

    update gold maker

--- seq2seq/eval_spider/format_gold.py
+++ seq2seq/eval_spider/format_gold.py
@@ -4,41 +4,42 @@ import json
 import os
 
 
-def format_gold(json_filename):
+def format_gold(json_filename, gold_filename):
     """
     Formats gold SQL queries from a .json file to a .sql file so they can be evaluated
     """
     gold_queries = []
 
-    with open(json_filename, 'r') as input_file:
+    with open(json_filename, "r") as input_file:
         all_instances = json.loads(input_file.read())
         gold_queries.extend(f"{p['query']}\t{p['db_id']}" for p in all_instances)
-    out_filename = os.path.join(os.path.dirname(json_filename), "dev_pig_specific_and_not_specific.sql")
+    out_filename = os.path.join(os.path.dirname(json_filename), gold_filename)
 
-    with open(out_filename, 'w') as output_file:
+    with open(out_filename, "w") as output_file:
         for q in gold_queries:
-            output_file.write(q + '\n')
+            output_file.write(q + "\n")
 
 
 def format_sql_field(json_filename, db_dir):
     """
     Creates a 'sql' field in the given .json file
     """
-    with open(json_filename, 'r') as input_file:
+    with open(json_filename, "r") as input_file:
         all_instances = json.loads(input_file.read())
 
     for i in all_instances:
-        db = os.path.join(db_dir, i['db_id'], i['db_id'] + ".sqlite")
+        db = os.path.join(db_dir, i["db_id"], i["db_id"] + ".sqlite")
         schema = Schema(get_schema(db))
-        if not i.get('sql'):
-            i['sql'] = get_sql(schema, i['query'])
+        if not i.get("sql"):
+            i["sql"] = get_sql(schema, i["query"])
 
-    with open(json_filename, 'w') as output_file:
+    with open(json_filename, "w") as output_file:
         output_file.write(json.dumps(all_instances))
 
 
 if __name__ == "__main__":
-    json_filename = "/app/dataset_files/seoss/dev_pig_specific_and_not_specific.json"
+    json_filename = "/app/dataset_files/seoss/dev_spider_plus_pig_specific_and_not_specific.json"
     db_dir = "/app/dataset_files/seoss/database"
+    gold_filename = "dev_spider_plus_pig_specific_and_not_specific.sql"
     format_sql_field(json_filename, db_dir)
-    format_gold(json_filename)
+    format_gold(json_filename, gold_filename)
